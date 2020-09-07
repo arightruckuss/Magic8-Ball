e1128506b471d905bc17d0258cdeb961476114b5
arightruckuss <66334071+arightruckuss@users.noreply.github.com>
2020-09-07 11:18:38 +0100

    Add files via upload

---
+++ Magic8.java
@@ -0,0 +1,74 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Magic8{
+  public static void main(String[] args){
+    System.out.println("Fortune telling Magic 8-Ball\n\nThink of a 'Yes' or  'No' question....\n\n");
+
+    Random rand = new Random();
+    int quesitonNumber = rand.nextInt(19);
+
+    switch(quesitonNumber){
+      case 0:
+      System.out.println("It is certain.");
+      break;
+      case 1:
+      System.out.println("As I see it, yes.");
+      break;
+      case 2:
+      System.out.println("Reply hazy, try again.");
+      break;
+      case 3:
+      System.out.println("Don't count on it");
+      break;
+      case 4:
+      System.out.println("It is decidedly so.");
+      break;
+      case 5:
+      System.out.println("Most likely.");
+      break;
+      case 6:
+      System.out.println("Ask again later.");
+      break;
+      case 7:
+      System.out.println("My reply is no.");
+      break;
+      case 8:
+      System.out.println("Without a doubt.");
+      break;
+      case 9:
+      System.out.println("Outlook good.");
+      break;
+      case 10:
+      System.out.println("Better not tell you now.");
+      break;
+      case 11:
+      System.out.println("My sources say no.");
+      break;
+      case 12:
+      System.out.println("Yes – definitely.");
+      break;
+      case 13:
+      System.out.println("Yes.");
+      break;
+      case 14:
+      System.out.println("Cannot predict now.");
+      break;
+     case 15:
+      System.out.println("Outlook not so good.");
+      break;
+      case 16:
+      System.out.println("You may rely on it.");
+      break;
+      case 17:
+      System.out.println("Signs point to yes.");
+      break;
+      case 18:
+      System.out.println("Concentrate and ask again.");
+      break;
+      case 19:
+      System.out.println("Very doubtful.");
+      break;
+      }
+    }
+  }
